f30901ced50701872d8f424cf376e9c8edb24661
Keshav <keshavgarg8800@gmail.com>
2025-06-24 09:44:33 +0530

    js learn

---
+++ 24-06-2025/index.js
@@ -0,0 +1,88 @@
+// let arr = ["a", "b", "c", "d", "e"];
+// for(let i =0; i < arr.length; i++) {
+//     console.log(arr[i]);
+// }
+
+//
+
+// let i;
+// if(i % 3 ==0 || i % 5 == 0){
+//     console.log("i is divisible by 3 or 5");
+// }
+
+// for(let i = 1; i <= 20; i++) {
+//     if(i % 4 == 0){
+//         console.log(i);
+//     }
+// }
+
+// let i;
+// let result = (i % 2 == 0) ? "Even" : "Odd";
+// console.log(result);
+
+// let i = 22;
+// if(i >= 18){
+//     console.log('Eligible to vote');
+// }
+
+// let i =85;;
+// if(i >= 90){
+//     console.log('Grade A');
+// }
+// else if(i>=75 && i < 90){
+//     console.log('Grade B');
+// }
+// else if(i >= 50 && i < 75){
+//     console.log('Grade C');
+// }
+// else if(i <50){
+//     console.log('Grade F');
+// }
+
+// let i = 2;
+
+// if (i > 0) {
+//     console.log('Positive');
+// }
+// else if (i < 0) {
+//     console.log('Negative');
+// }
+// else {
+//     console.log('Zero');
+// }
+
+// let i =14;
+// if(i % 2 == 0){
+//     if(i > 10) {
+//         console.log('Even and greater than 10');
+//     }
+// }
+
+// for(let i = 1; i<=10;i++){
+//     if(i == 5) continue;
+//     console.log(i);
+// }
+
+// let result;
+// for(let i = 1; i <= 10; i++){
+//     result += i + " ";
+// }
+// console.log(result)
+
+// let i= 1;
+// do{
+//     console.log("Welcome");
+//     i++;
+// }
+// while(i <= 5);
+
+// let isLoggedIn = true;
+// let result = isLoggedIn ? "Welcome back!" : "Please log in";
+// console.log(result);
+
+for(let i =1; i <=3; i++){
+   
+        result = i + " "+ i;
+
+    console.log(result);
+}
